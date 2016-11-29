2da2a54f0a54676fbb0a049ef4272ff7a50814fd
maisab <maaisa.barreto@gmail.com>
2016-11-29 00:46:56 -0200

    interface

---
+++ DocumentosTecnicos/clube_socios/app/Models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace clubeSocios\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    //
+
+    protected $fillable = [
+    'name'	
+    ];
+}
---
+++ DocumentosTecnicos/clube_socios/database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use clubeSocios\Models\Category;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Category::class, 10)->create();
+    }
+}
